f93dc87e37e90685cfe32b08afb50698dae13670
Nicolas Fischer <nicolas.fischer@netapsys.fr>
2017-10-03 07:24:05 +0200

    JSON to SQL script

--- json-to-sql.py
+++ json-to-sql.py
@@ -9,6 +9,11 @@ def loadJSON(filename):
         return json.loads(open(filename).read())
     return -1
 
+def getTeamIdByName(teamName):
+    cursor.execute("SELECT id FROM team WHERE name='" + teamName + "';")
+    return cursor.fetchone();
+
+
 season = "2010-2011"
 directory = "/var/www/basketball_reference/matches/united_states/nba/" + season
 
@@ -21,18 +26,27 @@ haNode = {
 conn = mysql.connector.connect(host="localhost",user="root",password="xy46oi03", database="NBA-games")
 cursor = conn.cursor()
 
-for filename in os.listdir(directory):
+fileList = sorted(os.listdir(directory))
+
+for filename in fileList:
     jsonData = loadJSON(directory + '/' + filename)
     if jsonData != -1:
         sqlQueryColumns = "INSERT INTO game("
         sqlQueryValues = " VALUES("
 
+        # Game winner
+        hPTS = jsonData["home"]["totals"]["PTS"]
+        aPTS = jsonData["away"]["totals"]["PTS"]
+        winner = 'H'
+        if aPTS > hPTS:
+            winner = 'A'
+
         for node in gameNode:
             sqlQueryColumns += node + ","
             sqlQueryValues += "'" + jsonData[node] + "',"
 
-        sqlQueryColumns = sqlQueryColumns[:-1] + ")"
-        sqlQueryValues = sqlQueryValues[:-1] + ");"
+        sqlQueryColumns += "winner)"
+        sqlQueryValues += "'" + winner + "');"
 
         #print sqlQueryColumns + sqlQueryValues
         cursor.execute(sqlQueryColumns + sqlQueryValues)
@@ -42,16 +56,28 @@ for filename in os.listdir(directory):
 
         # Stat
         for node in haNode:
-            sqlQueryColumns = "INSERT INTO stat(game_id,ha,"
-            sqlQueryValues = " VALUES(" + str(gameId) + ",'" + haNode[node] + "',"
+            teamId = getTeamIdByName(jsonData[node]["name"])
+
+            # Game result
+            result = 'W'
+            if (node == 'away' and aPTS < hPTS) or (node == 'home' and hPTS < aPTS):
+                result = 'L'
+
+            sqlQueryColumns = "INSERT INTO stat(game_id,ha,team_id,result,"
+            sqlQueryValues = " VALUES(" + str(gameId) + ",'" + haNode[node] + "'," + str(teamId[0]) + ",'" + result +"',"
 
             for subNode in jsonData[node]["totals"]:
-                sqlQueryColumns += STATS[subNode] + ","
+                sqlQueryColumns += "`" + STATS[subNode] + "`,"
                 sqlQueryValues += "'" + str(jsonData[node]["totals"][subNode]) + "',"
 
-        print sqlQueryColumns
-        print sqlQueryValues
+            sqlQueryColumns = sqlQueryColumns[:-1] + ")"
+            sqlQueryValues = sqlQueryValues[:-1] + ");"
 
+            print sqlQueryColumns
+            print sqlQueryValues
+
+            cursor.execute(sqlQueryColumns + sqlQueryValues)
+   
         conn.commit()
 
         break
