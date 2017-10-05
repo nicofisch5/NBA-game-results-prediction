15ac64cce8017a443e7d4bd6ce09831daea0119f
Nicolas Fischer <nicolas.fischer@netapsys.fr>
2017-10-05 14:57:03 +0200

    Prepare data with W/L ratio

--- prepare_data.py
+++ prepare_data.py
@@ -1,3 +1,4 @@
+import csv
 from init import session
 from game import Game
 from team import Team
@@ -5,7 +6,69 @@ from score import Score
 from team_stat import TeamStat
 
 
-rs = session.query(TeamStat).all()
+def get_team_ids_by_game_id(game_id):
+    team_ids = {}
+    rs = session.query(TeamStat).filter_by(game_id=game_id)
+    for obj in rs:
+        team_ids[obj.ha] = obj.team_id
 
-for obj in rs:
-    print obj.game_id, obj.team_id, obj.ha, obj.result, obj.STLP
+    return team_ids
+
+
+def get_wl_ratio_by_team_id(team_id, game_id):
+    wl_ratio = {}
+    for wl in ['W','L']:
+        rs = session.query(TeamStat, Game) \
+            .join(Game)\
+            .filter(TeamStat.team_id == team_id)\
+            .filter(TeamStat.result == wl)\
+            .filter(Game.type == 'Season') \
+            .filter(Game.id < game_id) \
+            .count()
+
+        wl_ratio[wl] = rs
+
+    return wl_ratio
+
+
+# For each game
+# H_won_ratio
+# A_Win_ratio
+# Div_Conf_Ext
+# Result
+
+data = []
+index = -1
+
+#rs = session.query(TeamStat).all()
+rs = session.query(Game).filter_by(type='Season')
+
+for game in rs:
+    index += 1
+    data.append(index)
+    data[index] = {}
+    #print obj.game_id, obj.team_id, obj.ha, obj.result, obj.STLP
+
+    data[index]['dce'] = game.dce
+    data[index]['winner'] = game.winner
+
+    # Get team_id of each team
+    team_ids = get_team_ids_by_game_id(game.id)
+
+    # Get WL ratio
+    hWL = get_wl_ratio_by_team_id(team_ids['H'], game.id)
+    aWL = get_wl_ratio_by_team_id(team_ids['A'], game.id)
+
+    print session.query(Team).get(team_ids['H']).code, hWL, " - ", aWL, session.query(Team).get(team_ids['A']).code
+
+    if index == 100:
+        break
+
+#ofile = open('ttest.csv', "wb")
+#writer = csv.writer(ofile, delimiter='', quotechar='"', quoting=csv.QUOTE_ALL)
+
+#for row in reader:
+#    writer.writerow(row)
+
+
+#ofile.close()
